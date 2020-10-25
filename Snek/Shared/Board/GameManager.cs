55c0eceba9cbcf863c1124518b6815fbcc606ddd
TautvydasD <tautvydas.diksas@gmail.com>
2020-10-25 20:20:37 +0200

    added some styling

--- Snek/Shared/Board/GameManager.cs
+++ Snek/Shared/Board/GameManager.cs
@@ -12,15 +12,15 @@ namespace Snek.Shared.Board
         private readonly int _speed = 200;
         public event EventHandler MainLoopCompleted;
 
-        public bool IsMovingUp { get; set; } = false;
-        public bool IsMovingDown { get; set; } = false;
-        public bool IsMovingRight { get; set; } = false;
-        public bool IsMovingLeft { get; set; } = false;
+        //public bool IsMovingUp { get; set; } = false;
+        //public bool IsMovingDown { get; set; } = false;
+        //public bool IsMovingRight { get; set; } = false;
+        //public bool IsMovingLeft { get; set; } = false;
 
         public bool IsRunning { get; private set; } = false;
 
-        public Coordinates pos { get; private set; }
-
+        //public Coordinates pos { get; private set; }
+        public Snake snake { get; set; }
         public GameManager()
         {
             ResetGameObjects();
@@ -32,61 +32,62 @@ namespace Snek.Shared.Board
             while (IsRunning)
             {
                 Transition();
-                MovingDirection();
+                //MovingDirection();
+                snake.MovingDirection();
                 MainLoopCompleted?.Invoke(this, EventArgs.Empty);
                 await Task.Delay(_speed);
             }
         }
-        public void MovingDirection()
-        {
-            if (IsMovingUp)
-                pos.Row--;
-            else if (IsMovingDown)
-                pos.Row++;
-            else if (IsMovingLeft)
-                pos.Column--;
-            else if (IsMovingRight)
-                pos.Column++;
-        }
+        //public void MovingDirection()
+        //{
+        //    if (IsMovingUp)
+        //        pos.Row--;
+        //    else if (IsMovingDown)
+        //        pos.Row++;
+        //    else if (IsMovingLeft)
+        //        pos.Column--;
+        //    else if (IsMovingRight)
+        //        pos.Column++;
+        //}
 
-        public void MoveUp()
-        {
-            IsMovingUp = true;
-            IsMovingDown = false;
-            IsMovingLeft = false;
-            IsMovingRight = false;
-        }
-        public void MoveDown()
-        {
-            IsMovingDown = true;
-            IsMovingUp = false;
-            IsMovingLeft = false;
-            IsMovingRight = false;
-        }
-        public void MoveLeft()
-        {
-            IsMovingLeft = true;
-            IsMovingRight = false;
-            IsMovingDown = false;
-            IsMovingUp = false;
-        }
-        public void MoveRight()
-        {
-            IsMovingRight = true;
-            IsMovingLeft = false;
-            IsMovingDown = false;
-            IsMovingUp = false;
-        }
+        //public void MoveUp()
+        //{
+        //    IsMovingUp = true;
+        //    IsMovingDown = false;
+        //    IsMovingLeft = false;
+        //    IsMovingRight = false;
+        //}
+        //public void MoveDown()
+        //{
+        //    IsMovingDown = true;
+        //    IsMovingUp = false;
+        //    IsMovingLeft = false;
+        //    IsMovingRight = false;
+        //}
+        //public void MoveLeft()
+        //{
+        //    IsMovingLeft = true;
+        //    IsMovingRight = false;
+        //    IsMovingDown = false;
+        //    IsMovingUp = false;
+        //}
+        //public void MoveRight()
+        //{
+        //    IsMovingRight = true;
+        //    IsMovingLeft = false;
+        //    IsMovingDown = false;
+        //    IsMovingUp = false;
+        //}
         public void Transition()
         {
-            if (pos.Column > 9)
-                pos.Column = -1;
-            else if (pos.Column < 0)
-                pos.Column = 10;
-            else if (pos.Row > 9)
-                pos.Row = -1;
-            else if (pos.Row < 0)
-                pos.Row = 10;
+            if (snake.pos.Column > 9)
+                snake.pos.Column = -1;
+            else if (snake.pos.Column < 0)
+                snake.pos.Column = 10;
+            else if (snake.pos.Row > 9)
+                snake.pos.Row = -1;
+            else if (snake.pos.Row < 0)
+                snake.pos.Row = 10;
         }
         public void StartGame()
         {
@@ -102,7 +103,8 @@ namespace Snek.Shared.Board
         }
         private void ResetGameObjects()
         {
-            pos = new Coordinates(1, 1);
+            snake = new Snake(1, 1);
+            //pos = new Coordinates(1, 1);
         }
     }
 }
---
+++ Snek/Shared/Board/Snake.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snek.Shared.Board
+{
+    public class Snake
+    {
+        public bool IsMovingUp { get; set; } = false;
+        public bool IsMovingDown { get; set; } = false;
+        public bool IsMovingRight { get; set; } = false;
+        public bool IsMovingLeft { get; set; } = false;
+
+        public Coordinates pos { get; set; }
+
+        public Snake() { }
+        public Snake(int X, int Y)
+        {
+            pos = new Coordinates(X, Y);
+        }
+        public void MovingDirection()
+        {
+            if (IsMovingUp)
+                pos.Row--;
+            else if (IsMovingDown)
+                pos.Row++;
+            else if (IsMovingLeft)
+                pos.Column--;
+            else if (IsMovingRight)
+                pos.Column++;
+        }
+        public void MoveUp()
+        {
+            IsMovingUp = true;
+            IsMovingDown = false;
+            IsMovingLeft = false;
+            IsMovingRight = false;
+        }
+        public void MoveDown()
+        {
+            IsMovingDown = true;
+            IsMovingUp = false;
+            IsMovingLeft = false;
+            IsMovingRight = false;
+        }
+        public void MoveLeft()
+        {
+            IsMovingLeft = true;
+            IsMovingRight = false;
+            IsMovingDown = false;
+            IsMovingUp = false;
+        }
+        public void MoveRight()
+        {
+            IsMovingRight = true;
+            IsMovingLeft = false;
+            IsMovingDown = false;
+            IsMovingUp = false;
+        }
+    }
+}
