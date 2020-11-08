e8fa4d79fc4510f4e2a016deef2af67746b8e06e
gedaus <gedis261@gmail.com>
2020-11-08 20:10:15 +0200

    Adapter

---
+++ Snek/Shared/Entities/Bomb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Snek.Shared.Board;
+
+namespace Snek.Shared.Entities
+{
+    public class Bomb
+    {
+        public string Image = "doublePoints.png";
+        public Bomb()
+        {
+
+        }
+        public int Explode()
+        {
+            return -1;
+        }
+    }
+}
---
+++ Snek/Shared/Entities/BombAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snek.Shared.Entities
+{
+    public class BombAdapter : Food
+    {
+        private Bomb bomb = new Bomb();
+        public BombAdapter() : base()
+        {
+
+        }
+        public override int LifeCount
+        {
+            get { return bomb.Explode(); }
+        }
+        public override string Image
+        {
+            get { return bomb.Image; }
+        }
+    }
+}
