c2a173d91a829951408bd42bcb0170e0222362e8
aisnor <noraistis@gmail.com>
2020-12-06 16:21:58 +0200

    Chain of Responsibility pattern added
    CoR is used to count player score. Normal food adds 1, special food adds 2, poison food subtracts 1, bomb subtracts 2. This handling is set up in a chain.

---
+++ Snek/Shared/Board/NoFoodCounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snek.Shared.Board
+{
+    public class NoFoodCounter : PointCounter
+    {
+        public NoFoodCounter(string name) : base(name)
+        {
+
+        }
+
+        public override void Calculate(string image)
+        {
+            Console.WriteLine("Bad input");
+        }
+
+        public override void SetNextChain(PointCounter next)
+        {
+            Next = next;
+        }
+
+    }
+}
---
+++ Snek/Shared/Board/PointCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snek.Shared.Board
+{
+    public abstract class PointCounter
+    {
+        protected static int PointSum = 0;
+        protected PointCounter Next;
+        protected string Name;
+        protected int Value;
+
+        public PointCounter(string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Calculate(string image);
+        public abstract void SetNextChain(PointCounter Next);
+        public int GetSum()
+        {
+            return PointSum;
+        }
+        public void SetSum(int newSum)
+        {
+            PointSum = newSum;
+        }
+    }
+}
